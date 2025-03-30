97d4e0dad64a46a4e7045dcdff4906aba9b85ce6
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-03-30 18:50:41 +0530

    Changes

--- src/MasterSemanticKernelInCS/SKKernelDemo/Program.cs
+++ src/MasterSemanticKernelInCS/SKKernelDemo/Program.cs
@@ -11,61 +11,61 @@ using System.Text;
 #pragma warning disable CA1303
 
 // Load configuration
-var config = new SemanticKernelConfig();
+//var config = new SemanticKernelConfig();
 
-Kernel openAIKernel = Kernel.CreateBuilder()
-    .AddOpenAIChatCompletion(config.OpenAIModel, config.OpenAIKey)
-    .Build();
+//Kernel openAIKernel = Kernel.CreateBuilder()
+//    .AddOpenAIChatCompletion(config.OpenAIModel, config.OpenAIKey)
+//    .Build();
 
-IKernelBuilder azureKernelBuilder = Kernel.CreateBuilder()
-    .AddAzureOpenAIChatCompletion(config.AzureModel, config.AzureEndpoint, config.AzureKey);
+//IKernelBuilder azureKernelBuilder = Kernel.CreateBuilder()
+//    .AddAzureOpenAIChatCompletion(config.AzureModel, config.AzureEndpoint, config.AzureKey);
 
-// Add logging
-azureKernelBuilder.Services.AddLogging(services => services.AddConsole().SetMinimumLevel(LogLevel.Trace));
+//// Add logging
+//azureKernelBuilder.Services.AddLogging(services => services.AddConsole().SetMinimumLevel(LogLevel.Trace));
 
-// Build the kernel
-Kernel azureKernel = azureKernelBuilder.Build();
+//// Build the kernel
+//Kernel azureKernel = azureKernelBuilder.Build();
 
-#region Chat Completion
-var options = new OpenAIPromptExecutionSettings
-{
-    MaxTokens = 150,
-    Temperature = 0.9
-};
+//#region Chat Completion
+//var options = new OpenAIPromptExecutionSettings
+//{
+//    MaxTokens = 150,
+//    Temperature = 0.9
+//};
 
-var prompt = "Write a short poem about Semantic Kernel";
+//var prompt = "Write a short poem about Semantic Kernel";
 
-// Open AI
-var openAIResult = await openAIKernel.InvokePromptAsync(prompt, new KernelArguments(options)).ConfigureAwait(false);
-ForegroundColor = ConsoleColor.DarkCyan;
-WriteLine(openAIResult);
+//// Open AI
+//var openAIResult = await openAIKernel.InvokePromptAsync(prompt, new KernelArguments(options)).ConfigureAwait(false);
+//ForegroundColor = ConsoleColor.DarkCyan;
+//WriteLine(openAIResult);
 
-// Azure Open AI
-var azOpenAIResult = await azureKernel.InvokePromptAsync(prompt, new KernelArguments(options)).ConfigureAwait(false);
-ForegroundColor = ConsoleColor.DarkYellow;
-WriteLine($"\n\n{azOpenAIResult}");
-#endregion
+//// Azure Open AI
+//var azOpenAIResult = await azureKernel.InvokePromptAsync(prompt, new KernelArguments(options)).ConfigureAwait(false);
+//ForegroundColor = ConsoleColor.DarkYellow;
+//WriteLine($"\n\n{azOpenAIResult}");
+//#endregion
 
-#region Chat Completion Streaming
+//#region Chat Completion Streaming
 
-ForegroundColor = ConsoleColor.DarkMagenta;
+//ForegroundColor = ConsoleColor.DarkMagenta;
 
-var fullMessageBuilder = new StringBuilder();
+//var fullMessageBuilder = new StringBuilder();
 
-await foreach (var chatUpdate in azureKernel.InvokePromptStreamingAsync<StreamingChatMessageContent>(prompt).ConfigureAwait(false))
-{
-    if (!string.IsNullOrEmpty(chatUpdate.Content))
-    {
-        fullMessageBuilder.Append(chatUpdate.Content);
-        Write(chatUpdate.Content);
-    }
-}
+//await foreach (var chatUpdate in azureKernel.InvokePromptStreamingAsync<StreamingChatMessageContent>(prompt).ConfigureAwait(false))
+//{
+//    if (!string.IsNullOrEmpty(chatUpdate.Content))
+//    {
+//        fullMessageBuilder.Append(chatUpdate.Content);
+//        Write(chatUpdate.Content);
+//    }
+//}
 
-ForegroundColor = ConsoleColor.Green;
-string fullMessage = fullMessageBuilder.ToString();
-WriteLine($"\n\n{fullMessage}");
+//ForegroundColor = ConsoleColor.Green;
+//string fullMessage = fullMessageBuilder.ToString();
+//WriteLine($"\n\n{fullMessage}");
 
-#endregion
+//#endregion
 
 ResetColor();
 WriteLine("\n\nPress any key to exit...");

0b151e4e9c79d27c9e64f08aec647702a0db2d87
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-03-30 18:45:36 +0530

    Create IAzurePromptService.cs

---
+++ src/MasterSemanticKernelInCS/SKKernelDemo/Services/IAzurePromptService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKKernelDemo.Services;
+
+internal interface IAzurePromptService
+{
+    Task<string?> GetPromptResponseAsync(string prompt);
+
+    IAsyncEnumerable<string?> StreamPromptResponseAsync(string prompt);
+}

408b70aa6ac540d54d6a3a9e45c72100c881c139
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-04-02 22:34:27 +0530

    Update Program.cs

--- src/MasterSemanticKernelInCS/SKKernelDemo/Program.cs
+++ src/MasterSemanticKernelInCS/SKKernelDemo/Program.cs
@@ -1,50 +1,67 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using SKKernelDemo.Infrastructure;
 using SKKernelDemo.Services;
+using System.Text;
 
 #pragma warning disable SKEXP0010
 #pragma warning disable SKEXP0001
 #pragma warning disable S125
 #pragma warning disable CA1303
 
+Console.OutputEncoding = Encoding.UTF8;
 
 var host = HostBuilderFactory.BuildHost(args);
 
 var openAiService = host.Services.GetRequiredService<IOpenAIPromptService>();
-string prompt = "What is an apple?";
 
-WriteLine($"Prompt: {prompt}");
-ForegroundColor = ConsoleColor.DarkCyan;
+// Ask user for input
+Write("Enter your prompt: ");
+string? prompt = ReadLine();
+
+if (string.IsNullOrWhiteSpace(prompt))
+{
+    WriteLine("Prompt cannot be empty. Exiting...");
+    return;
+}
+
+WriteLine($"\nPrompt: {prompt}");
+
 WriteLine("\n******************** OpenAI Response: ********************");
+ForegroundColor = ConsoleColor.DarkCyan;
 string? openAiResponse = await openAiService.GetPromptResponseAsync(prompt).ConfigureAwait(false);
 WriteLine(openAiResponse);
-
 ResetColor();
+WriteLine("\n-------------------- OpenAI Response --------------------");
+
 WriteLine($"\n\nPrompt: {prompt}");
-ForegroundColor = ConsoleColor.Magenta;
 WriteLine("\n******************** OpenAI Streaming Response: ********************");
+ForegroundColor = ConsoleColor.Magenta;
 await foreach (var chunk in openAiService.StreamPromptResponseAsync(prompt).ConfigureAwait(false))
 {
     Write(chunk);
 }
+ResetColor();
+WriteLine("\n-------------------- OpenAI Response --------------------");
 
 var azureService = host.Services.GetRequiredService<IAzurePromptService>();
 
-ResetColor();
 WriteLine($"\n\nPrompt: {prompt}");
-ForegroundColor = ConsoleColor.DarkYellow;
 WriteLine("\n******************** Azure Response: ********************");
+ForegroundColor = ConsoleColor.DarkYellow;
 string? azureResponse = await azureService.GetPromptResponseAsync(prompt).ConfigureAwait(false);
 WriteLine(azureResponse);
-
 ResetColor();
+WriteLine("\n-------------------- OpenAI Response --------------------");
+
 WriteLine($"\n\nPrompt: {prompt}");
-ForegroundColor = ConsoleColor.Green;
 WriteLine("\n******************** Azure Streaming Response: ********************");
+ForegroundColor = ConsoleColor.Green;
 await foreach (var chunk in azureService.StreamPromptResponseAsync(prompt).ConfigureAwait(false))
 {
     Write(chunk);
 }
+ResetColor();
+WriteLine("\n-------------------- OpenAI Response --------------------");
 
 ResetColor();
 WriteLine("\n\nPress any key to exit...");

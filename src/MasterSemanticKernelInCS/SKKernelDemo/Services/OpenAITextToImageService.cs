0673940b57ba4bbde8b828371f1559be7f95a791
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-04-04 23:27:19 +0530

    Update OpenAITextToImageService.cs

--- src/MasterSemanticKernelInCS/SKKernelDemo/Services/OpenAITextToImageService.cs
+++ src/MasterSemanticKernelInCS/SKKernelDemo/Services/OpenAITextToImageService.cs
@@ -1,15 +1,1 @@
-﻿using Microsoft.SemanticKernel;
-using Microsoft.SemanticKernel.Connectors.OpenAI;
-using SKKernelDemo.Kernels;
-
-namespace SKKernelDemo.Services;
-
-internal sealed class OpenAITextToImageService(OpenAIKernelWrapper kernelWrapper) : ITextToImageService
-{
-    private readonly Kernel _kernel = kernelWrapper.Kernel;
-
-    public async Task<IReadOnlyList<ImageContent>> GetImageContentsAsync(string prompt, OpenAITextToImageExecutionSettings options)
-    {
-        return await _kernel.InvokeTextToImageAsync(prompt, executionSettings: options).ConfigureAwait(false);
-    }
-}
+﻿

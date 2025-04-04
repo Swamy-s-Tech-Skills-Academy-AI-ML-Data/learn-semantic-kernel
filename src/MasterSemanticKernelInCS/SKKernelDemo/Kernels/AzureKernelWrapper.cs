d8e632b7af35ba29405eadde69220034c4b84cc2
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-04-04 15:38:17 +0530

    Added DALLE

--- src/MasterSemanticKernelInCS/SKKernelDemo/Kernels/AzureKernelWrapper.cs
+++ src/MasterSemanticKernelInCS/SKKernelDemo/Kernels/AzureKernelWrapper.cs
@@ -5,6 +5,8 @@ using SKKernelDemo.Configuration;
 namespace SKKernelDemo.Kernels;
 
 #pragma warning disable S125
+#pragma warning disable CA1848
+#pragma warning disable SKEXP0010
 
 internal sealed class AzureKernelWrapper
 {
@@ -12,8 +14,11 @@ internal sealed class AzureKernelWrapper
 
     public AzureKernelWrapper(SemanticKernelConfig config, ILogger<AzureKernelWrapper> logger)
     {
+        logger.BeginScope("AzureKernelWrapper");
+
         var builder = Kernel.CreateBuilder()
-            .AddAzureOpenAIChatCompletion(config.AzureModel, config.AzureEndpoint, config.AzureKey);
+            .AddAzureOpenAIChatCompletion(config.AzureModel, config.AzureEndpoint, config.AzureKey)
+            .AddAzureOpenAITextToImage("dall-e-3", config.AzureEndpoint, config.AzureKey);
 
         //builder.Services.AddLogging(logging =>
         //{
@@ -21,5 +26,7 @@ internal sealed class AzureKernelWrapper
         //});
 
         Kernel = builder.Build();
+
+        logger.LogInformation("Azure Kernel initialized with model: {Model}", config.AzureModel);
     }
 }

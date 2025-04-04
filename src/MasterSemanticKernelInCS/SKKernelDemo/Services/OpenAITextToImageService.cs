7aa626afdfc0b40440fa9e64c779a155343a5264
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-04-04 15:59:37 +0530

    Code Refactoring

---
+++ src/MasterSemanticKernelInCS/SKKernelDemo/Services/OpenAITextToImageService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using SKKernelDemo.Kernels;
+
+namespace SKKernelDemo.Services;
+
+internal sealed class OpenAITextToImageService(OpenAIKernelWrapper kernelWrapper) : ITextToImageService
+{
+    private readonly Kernel _kernel = kernelWrapper.Kernel;
+
+    public async Task<IReadOnlyList<ImageContent>> GetImageContentsAsync(string prompt, OpenAITextToImageExecutionSettings options)
+    {
+        return await _kernel.InvokeTextToImageAsync(prompt, executionSettings: options).ConfigureAwait(false);
+    }
+}

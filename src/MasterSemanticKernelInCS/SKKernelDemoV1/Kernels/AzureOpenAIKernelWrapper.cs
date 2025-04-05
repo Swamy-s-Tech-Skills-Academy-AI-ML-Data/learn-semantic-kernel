57c02c4d719645f3f009165d424d00fdabc17631
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-04-05 22:01:19 +0530

    Copied the files

---
+++ src/MasterSemanticKernelInCS/SKKernelDemoV1/Kernels/AzureOpenAIKernelWrapper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using SKKernelDemo.Configuration;
+
+namespace SKKernelDemo.Kernels;
+
+#pragma warning disable S125
+#pragma warning disable CA1848
+#pragma warning disable SKEXP0010
+
+internal sealed class AzureOpenAIKernelWrapper
+{
+    public Kernel Kernel { get; }
+
+    public AzureOpenAIKernelWrapper(SemanticKernelConfig config, ILogger<AzureOpenAIKernelWrapper> logger)
+    {
+        logger.BeginScope(nameof(AzureOpenAIKernelWrapper));
+
+        var builder = Kernel.CreateBuilder()
+            .AddAzureOpenAIChatCompletion(config.AzureModel, config.AzureEndpoint, config.AzureKey);
+
+        //builder.Services.AddLogging(logging =>
+        //{
+        //    logging.AddConsole().SetMinimumLevel(LogLevel.Trace);
+        //});
+
+        Kernel = builder.Build();
+
+        logger.LogInformation("Azure OpenAI Kernel initialized with model: {Model}", config.AzureModel);
+    }
+}
---
+++ src/MasterSemanticKernelInCS/SKKernelDemoV1/Kernels/OpenAIKernelWrapper.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.SemanticKernel;
+using SKKernelDemo.Configuration;
+
+namespace SKKernelDemo.Kernels;
+
+#pragma warning disable SKEXP0010
+
+internal sealed class OpenAIKernelWrapper(SemanticKernelConfig config)
+{
+    public Kernel Kernel { get; } = Kernel.CreateBuilder()
+            .AddOpenAIChatCompletion(config.OpenAIModel, config.OpenAIKey)
+            .Build();
+}
